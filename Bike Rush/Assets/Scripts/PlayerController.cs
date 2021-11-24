e48ca075e3de63ef0c8f777d88091203186ad2b7
VeroSisianu <nica220196@gmail.com>
2021-11-24 11:37:12 +0200

    CharacterController

---
+++ Bike Rush/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float velocityChanger;
+    public float VelocityDelta = 0.2f;
+    public float MaxVelocity = 4f;
+    public float GrowVelocityX = 1000f;
+    Rigidbody rb;
+    Animator anim;
+
+    private Vector3 startPos;
+    private Vector3 currentPos;
+    private Vector2 bounds = new Vector2(32.5f, 38);
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        var clampedX = Mathf.Clamp(transform.position.x, bounds.x, bounds.y);
+        transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
+    }
+
+    private void FixedUpdate()
+    {
+        ReceiveInput();
+    }
+
+    private void ReceiveInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPos = ConvertPos(Input.mousePosition);
+            anim.speed = 2.5f;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            if (velocityChanger < MaxVelocity)
+            {
+                velocityChanger += VelocityDelta;
+            }
+            currentPos = ConvertPos(Input.mousePosition);
+            var deltaPos = currentPos - startPos;
+
+            SetVelocityDependingOnInput(deltaPos);
+        }
+        else
+        {
+            if (velocityChanger > 0)
+            {
+                velocityChanger -= VelocityDelta;
+                rb.velocity = new Vector3(0, 0, velocityChanger);
+            }
+            else if (!rb.IsSleeping())
+            {
+                anim.speed = 0;
+                rb.Sleep();
+            }
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            startPos = ConvertPos(Input.mousePosition);
+        }
+    }
+
+    private void SetVelocityDependingOnInput(Vector3 deltaPos)
+    {
+        if (deltaPos.x > 0 && transform.position.x < bounds.y)
+        {
+            rb.velocity = new Vector3(deltaPos.x * GrowVelocityX, 0, velocityChanger);
+            Debug.Log("a");
+        }
+        else if (deltaPos.x < 0 && transform.position.x > bounds.x)
+        {
+            rb.velocity = new Vector3(deltaPos.x * GrowVelocityX, 0, velocityChanger);
+            Debug.Log("b");
+        }
+        else
+        {
+            rb.velocity = new Vector3(0, 0, velocityChanger);
+        }
+    }
+
+    public Vector3 ConvertPos(Vector3 position)
+    {
+        return new Vector3(position.x / Screen.width, 0, 0);
+    }
+}
